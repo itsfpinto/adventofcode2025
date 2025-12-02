aa1a511d32d7c543a0a672df468651a61524514a
itsfpinto <daraujo.filipe@gmail.com>
2025-12-02 12:50:10 +0000

    Day 1 - 1 star

---
+++ dayone.py
@@ -0,0 +1,28 @@
+dir_list = []
+amount_list = []
+
+with open("adventofcode_day1_input", "r") as file:
+    for line in file:
+        if line.strip():
+            dir_list.append(line.strip()[0])
+            amount_list.append(int(line[1:]))
+
+pos = 50
+number_of_zeros = 0
+
+def loop (nr):
+    return nr % 100
+
+for i in range(len(amount_list)):
+    if dir_list[i] == "R":
+        dial_movement = amount_list[i]
+        pos += dial_movement
+        pos = loop(pos)
+    if dir_list[i] == "L":
+        dial_movement = amount_list[i]
+        pos -= dial_movement
+        pos = loop(pos)
+    if pos == 0:
+        number_of_zeros += 1
+
+print(number_of_zeros)
